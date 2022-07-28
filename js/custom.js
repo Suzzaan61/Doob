ebe2ba057b59714d35e22f4431fd1180a74980b8
suzan <sznghimire61@gmail.com>
2022-07-28 10:43:36 -0700

    aa

--- js/custom.js
+++ js/custom.js
@@ -33,3 +33,20 @@ $('.section4-slider').owlCarousel({
         }
     }
 })
+$('.section6-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
