7a723e481c0945d30b831fa972699ad9c682f2cd
suzan <sznghimire61@gmail.com>
2022-07-28 10:08:57 -0700

    for other brance

---
+++ js/custom.js
@@ -0,0 +1,35 @@
+var $ = jQuery.noConflict();
+$('.section2-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:2
+        }
+    }
+})
+$('.section4-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
